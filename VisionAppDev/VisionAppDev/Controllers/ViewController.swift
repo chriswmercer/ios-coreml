171a5311fb5b0ef9522929cf180c7011bb7421eb
Unknown <chriswm@me.com>
2020-04-25 17:17:32 +0100

    Lesson 4 - Project Setup

---
+++ VisionAppDev/VisionAppDev/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VisionAppDev
+//
+//  Created by Chris Mercer on 25/04/2020.
+//  Copyright © 2020 Chris Mercer. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
